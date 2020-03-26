d605b9d0bf35c1f60e1b7dc2adbb304ba018a47f
Ilya Zhdanov <zdravulin@gmail.com>
2020-03-26 11:40:45 +0500

    complete

--- module_2/sql_example/app/src/main/java/com/example/sql_example/view/FriendsList.java
+++ module_2/sql_example/app/src/main/java/com/example/sql_example/view/FriendsList.java
@@ -1,17 +1,21 @@
 package com.example.sql_example.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.sql_example.R;
 import com.example.sql_example.domain.User;
 import com.example.sql_example.interactor.UsersInteractor;
+import com.example.sql_example.view.adapter.UsersListAdapter;
 
 public class FriendsList extends AppCompatActivity {
 
     User user;
     UsersInteractor interactor;
+    RecyclerView usersListRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,10 @@ public class FriendsList extends AppCompatActivity {
 
     private void init(){
         interactor = new UsersInteractor(this);
+        usersListRecyclerView = findViewById(R.id.users_list_recycler_view);
+        UsersListAdapter usersListAdapter = new UsersListAdapter(interactor.getUserList(user.id), user);
+        usersListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        usersListRecyclerView.setAdapter(usersListAdapter);
     }
 
     private String getUserName(){
